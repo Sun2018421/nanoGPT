7b19551e7692350533c3357340c1f65c0f11b0a8
XiaofengSun <sunxf@zju.edu.cn>
2025-06-10 17:13:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ASCEND_RT_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc_per_node=4 train_non_clip.py config/train_gpt2.py
+# 根据ASCENDXX来确定使用的NPU
+ASCEND_RT_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc_per_node=4 train.py config/train_gpt2.py
